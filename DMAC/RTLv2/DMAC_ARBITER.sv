0904e24198151caf5be989bc78c446b173e54d41
Jinhee Kim <a2jinhee@gmail.com>
2024-04-24 20:11:57 +0900

    .

--- DMAC/RTLv2/DMAC_ARBITER.sv
+++ DMAC/RTLv2/DMAC_ARBITER.sv
@@ -22,119 +22,135 @@ module DMAC_ARBITER
     output  reg     [DATA_SIZE-1:0] dst_data_o
 );
 
-    // TODO: implement your arbiter here
-    // src_valid_i : valid signal vector for each master = if 1, master has data to send
-    // src_data_i :data signal vector for each master = (id, data, strb, last)
-    // dst_ready_i :ready signal for selected slave = if 1, slave is ready to receive data
-
-    // src_ready_o :ready signal vector for each master = if 1, master is ready to send data
-    // dst_valid_o : valid signal for selected slave  = if 1, slave has data to receive
-    // dst_data_o : data signal for slave = (id, data, strb, last)
-
-    // Arbiter shoots src_ready_o=1 in a round robin way
-    // if src_valid_i=1 when arbiter shoots src_ready_o=1 to corresponding slave, arbiter selects the slave
+    reg     [3:0]                       sel;
+    reg                                 src_ready [N_MASTER];
+    reg                                 dst_valid,  dst_valid_n;
+    reg     [15:0]                      duty,   duty_n, duty_size;
+    reg     [3:0]                       queue[0:3], queue_n[0:3];
+    wire    [3:0]                       request = {src_valid_i[3],src_valid_i[2],src_valid_i[1],src_valid_i[0]};
     
-    // if dst_ready_i=1, arbiter shoots dst_valid_o=1 to the selected slave
-    // get data from the selected slave and send it to the dst_data_o
+    reg [DATA_SIZE-1:0]                 dst_data, dst_data_n;
 
-    // mnemonics for state values
-    localparam                  s_0  = 2'd0,
-                                s_1  = 2'd1,
-                                s_2  = 2'd2,
-                                s_3  = 2'd3;
-
-    reg     [1:0]               state,          state_n;
-    // reg     [31:0]              dst_data,       dst_data_n;
-    // reg                         dst_valid,      dst_valid_n;
-    // reg                         src_ready[N_MASTER], src_ready_n[N_MASTER];
-
-    // it's desirable to code registers in a simple way
-    always_ff @(posedge clk)
-        if (!rst_n) begin
-            state               <= s_0;
-            // dst_data            <= 32'd0;
-            // dst_valid           <= 1'b0;
-            // src_ready           <= '{N_MASTER{1'b0}};
+    always_ff @(posedge clk) begin
+        if(!rst_n) begin
+            duty                         <=  0;
+            duty_size                    <=  1;
+            queue                        <= {{4'b0001},{4'b0010},{4'b0100},{4'b1000}};          
+            dst_valid                    <= 1'b0;    
+            dst_data                     <=  'b0;
         end
         else begin
-            state               <= state_n;
-            // dst_data            <= dst_data_n;
-            // dst_valid           <= dst_valid_n;
-            // src_ready           <= src_ready_n;
-        end
+            queue                       <= queue_n;
+            duty                        <= duty_n;
+            dst_valid                   <= dst_valid_n;
+            dst_data                    <= dst_data_n;
+        end     
+/*      
+        $display("%d) src_valid_i : %d %d %d %d", iid, src_valid_i[0], src_valid_i[1], src_valid_i[2], src_valid_i[3]);
+        $display("%d) src_ready_o : %d %d %d %d", iid, src_ready[0], src_ready[1], src_ready[2], src_ready[3]);
+        $display("%d) dst_valid_o , dst_ready_i, dst_valid_n : %d %d %d",iid, dst_valid,dst_ready_i, dst_valid_n);
+        $display("%d) fifo_wdata, fifo_rdata, dst_data : %0x %0x %0x", iid, fifo_wdata, fifo_rdata, dst_data);
+        $display("%d) fifo : %0x %0x %0x %0x", iid, queue[0],queue[1],queue[2],queue[3]);
 
-    always_comb begin
-        state_n                 = state;
-        // dst_data_n              = dst_data;
-        // dst_valid_n             = dst_valid;
-        // src_ready_n             = src_ready;
-        dst_valid_o             = 0; 
-        src_ready_o             = '{N_MASTER{1'b0}};
-        
-        case (state)
-        s_0:     begin
-                dst_data_o = src_data_i[0];
-                dst_valid_o = src_valid_i[0] ? 1 : 0;
-				src_ready_o[0] = dst_ready_i ? 1 : 0;
-
-                if (!dst_valid_o) begin
-                    if      (src_valid_i[1]) begin state_n = s_1; end
-                    else if (src_valid_i[2]) begin state_n = s_2; end
-                    else if (src_valid_i[3]) begin state_n = s_3; end
-                    else if (src_valid_i[0]) begin state_n = s_0; end
-                    else state_n = s_0; 
-                end
-                end
-
-
-        s_1:    begin
-                dst_data_o = src_data_i[1];
-                dst_valid_o = src_valid_i[1] ? 1 : 0;
-				src_ready_o[1] = dst_ready_i ? 1 : 0;
-
-                if (!dst_valid_o) begin
-                    if      (src_valid_i[2]) begin state_n = s_2; end
-                    else if (src_valid_i[3]) begin state_n = s_3; end
-                    else if (src_valid_i[0]) begin state_n = s_0; end
-                    else if (src_valid_i[1]) begin state_n = s_1; end
-                    else state_n = s_1; 
-                end
-                end
-
-
-        s_2:    begin
-                dst_data_o = src_data_i[2];
-                dst_valid_o = src_valid_i[2] ? 1 : 0;
-				src_ready_o[2] = dst_ready_i ? 1 : 0;
-
-                if (!dst_valid_o) begin
-                    if      (src_valid_i[3]) begin state_n = s_3; end
-                    else if (src_valid_i[0]) begin state_n = s_0; end
-                    else if (src_valid_i[1]) begin state_n = s_1; end
-                    else if (src_valid_i[2]) begin state_n = s_2; end
-                    else state_n = s_2;
-                end
-                end
-
-
-        s_3:    begin
-                dst_data_o = src_data_i[3];
-                dst_valid_o = src_valid_i[3] ? 1 : 0;
-				src_ready_o[3] = dst_ready_i ? 1 : 0;
-
-                if (!dst_valid_o) begin
-                    if      (src_valid_i[0]) begin state_n = s_0; end
-                    else if (src_valid_i[1]) begin state_n = s_1; end
-                    else if (src_valid_i[2]) begin state_n = s_2; end
-                    else if (src_valid_i[3]) begin state_n = s_3; end
-                    else state_n = s_3;
-                    end
-                end
-        endcase
+        $display("%d) src_valid_i : %d %d %d %d", iid, src_valid_i[0], src_valid_i[1], src_valid_i[2], src_valid_i[3]);
+        $display("%d) src_ready_0 : %d %d %d %d", iid, src_ready[0], src_ready[1], src_ready[2], src_ready[3]);
+        $display("%d) dst_valid_o : %d", iid, dst_valid);
+        $display("%d) duty : %d", iid, duty);
+*/
     end
 
-    // assign  dst_data_o                = dst_data;
-    // assign  dst_valid_o               = dst_valid;
-    // assign  src_ready_o               = src_ready;
+    always_comb begin
+        duty_n                          =   duty ;
+        queue_n                         =   queue;
+        sel                             =   4'b0;
+        src_ready                       =   {{0},{0},{0},{0}};
+        dst_data_n                      =   dst_data;
+        dst_valid_n                     =   dst_valid;
+
+        if( !((queue[0] & request) == 4'b0000) )   begin
+            sel                         =   (queue[0] == 4'b0001) ? 0 :
+                                            (queue[0] == 4'b0010) ? 1 :
+                                            (queue[0] == 4'b0100) ? 2 : 3;
+            if( ((dst_ready_i == 1'b1) & (dst_valid == 1'b1)) | (dst_valid == 1'b0)) begin
+                src_ready[sel]              =   1;
+                dst_data_n                  =   src_data_i[sel];
+                dst_valid_n                 =   1'b1;       
+                if(duty == 0) begin
+                    duty_n                      =   duty_size;
+                    queue_n                     =   {{queue[1]},{queue[2]},{queue[3]},{queue[0]}};                
+                end         
+                else begin
+                    duty_n                      =   duty - 1;
+                end
+            end 
+        end
+        else if( !((queue[1] & request) == 4'b0000) )   begin
+            sel                         =   (queue[1] == 4'b0001) ? 0 :
+                                            (queue[1] == 4'b0010) ? 1 :
+                                            (queue[1] == 4'b0100) ? 2 : 3;
+            if( ((dst_ready_i == 1'b1) & (dst_valid == 1'b1)) | (dst_valid == 1'b0)) begin
+                src_ready[sel]              =   1;
+                dst_data_n                  =   src_data_i[sel];
+                dst_valid_n                 =   1'b1;                
+                if(duty == 0) begin
+                    duty_n                      =   duty_size;                
+                    queue_n                     =   {{queue[0]},{queue[2]},{queue[3]},{queue[1]}};           
+                end
+                else begin
+                    duty_n                      =   duty - 1;
+                end                
+            end
+        end
+        else if( !((queue[2] & request) == 4'b0000) )   begin
+            sel                         =   (queue[2] == 4'b0001) ? 0 :
+                                            (queue[2] == 4'b0010) ? 1 :
+                                            (queue[2] == 4'b0100) ? 2 : 3;
+            if( ((dst_ready_i == 1'b1) & (dst_valid == 1'b1)) | (dst_valid == 1'b0)) begin
+                src_ready[sel]              =   1;
+                dst_data_n                  =   src_data_i[sel];
+                dst_valid_n                 =   1'b1;
+                if(duty == 0) begin
+                    duty_n                      =   duty_size;                
+                    queue_n                     =   {{queue[0]},{queue[1]},{queue[3]},{queue[2]}};          
+                end
+                else begin
+                    duty_n                      =   duty - 1;
+                end                
+            end
+        end
+        else if( !((queue[3] & request) == 4'b0000) )   begin
+            sel                         =   (queue[3] == 4'b0001) ? 0 :
+                                            (queue[3] == 4'b0010) ? 1 :
+                                            (queue[3] == 4'b0100) ? 2 : 3;
+            if( ((dst_ready_i == 1'b1) & (dst_valid == 1'b1)) | (dst_valid == 1'b0)) begin
+                src_ready[sel]              =   1;
+                dst_data_n                  =   src_data_i[sel];
+                dst_valid_n                 =   1'b1;                
+                if(duty == 0) begin
+                    duty_n                      =   duty_size;                
+                    queue_n                     =   {{queue[0]},{queue[1]},{queue[2]},{queue[3]}};    
+                end
+                else begin
+                    duty_n                      =   duty - 1;
+                end                
+            end                
+        end
+        else if( (request == 4'b0000) & (dst_ready_i == 1'b1) & (dst_valid == 1'b1) ) begin
+            dst_valid_n                     =   1'b0;
+        end 
+       
+        if( (request == 4'b0000 ) | ((dst_ready_i == 1'b0) & (dst_valid == 1'b1 ) ) )begin
+           if(duty == 0)begin
+                duty_n                          =    'b0;
+            end
+            else begin
+                duty_n                          =    duty - 1;
+            end
+        end
+    end
+
+    assign  dst_valid_o         =   dst_valid;
+    assign  dst_data_o          =   dst_data;
+    assign  src_ready_o         =   src_ready;
 
 endmodule
